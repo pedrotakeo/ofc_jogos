9f0befecc91efaed5ed1cd34789db92f679b50bb
Pedro Takeo Shima <pedrotakeo@ufpr.br>
2026-03-10 11:26:40 -0300

    update readme

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <div class="fixed right-0 top-0 bg-black h-25 md w-screen">
     <div class="w-full h-full flex flex-row justify-center items-center">
       <div>
-        OFICINA DE JOGOS UFPR
+
       </div>
     </div>
   </div>
