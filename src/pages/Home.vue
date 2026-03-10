378d4c6b8cbc54be7f00c6a99f00ac842a8a91aa
Pedro Takeo Shima <pedrotakeo@ufpr.br>
2026-03-10 14:54:41 -0300

    adicionar sistema de paginas

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,8 +12,8 @@
 
 
     <div class="pt-10 w-full flex flex-col justify-center items-center px-10">
-      <div class="text-4xl">LOREM IPSUM</div>
-      <div class="text-xl pt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pharetra ullamcorper magna lacinia aliquet. Nullam elit lorem, ornare sit amet ultricies at, ornare sollicitudin nulla. Phasellus tempor urna massa, ac lobortis tellus gravida at. In imperdiet varius nunc, vitae efficitur leo viverra id. Donec fringilla et nibh sed eleifend. Sed dapibus condimentum elit. Ut est tortor, eleifend in vehicula nec, ultricies sit amet erat. Nullam vel risus lacus. Sed vehicula dui et lacus pulvinar vestibulum. Nullam tristique mi et nulla aliquet interdum. Quisque tincidunt sit amet nisl nec ullamcorper. Donec rutrum consequat augue sit amet tristique. Aliquam a neque sit amet ipsum sodales finibus non dapibus nisi. Fusce pellentesque consequat massa eu pretium. Donec faucibus metus ipsum, at tincidunt lectus commodo in. Morbi sagittis felis fringilla nisi consequat aliquet.</div>
+      <div class="text-4xl text-center">LOREM IPSUM</div>
+      <div class="text-md py-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pharetra ullamcorper magna lacinia aliquet. Nullam elit lorem, ornare sit amet ultricies at, ornare sollicitudin nulla. Phasellus tempor urna massa, ac lobortis tellus gravida at. In imperdiet varius nunc, vitae efficitur leo viverra id. Donec fringilla et nibh sed eleifend. Sed dapibus condimentum elit. Ut est tortor, eleifend in vehicula nec, ultricies sit amet erat. Nullam vel risus lacus. Sed vehicula dui et lacus pulvinar vestibulum. Nullam tristique mi et nulla aliquet interdum. Quisque tincidunt sit amet nisl nec ullamcorper. Donec rutrum consequat augue sit amet tristique. Aliquam a neque sit amet ipsum sodales finibus non dapibus nisi. Fusce pellentesque consequat massa eu pretium. Donec faucibus metus ipsum, at tincidunt lectus commodo in. Morbi sagittis felis fringilla nisi consequat aliquet.</div>
     </div>
 
 
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,16 @@
+import Membros from './pages/Membros.vue'
+import Home from './pages/Home.vue';
+import Projetos from './pages/Projetos.vue'
+import Inscricao from './pages/Inscricao.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './pages/Home.vue'
 
 const routes = [
   { path: '/', redirect: '/home' },
 
   { path: '/home', component: Home, meta: { public: true } },
+  { path: '/membros', component: Membros, meta: { public: true } },
+  { path: '/projetos', component: Projetos, meta: { public: true } },
+  { path: '/inscricao', component: Inscricao, meta: { public: true } },
   { path: '/:pathMatch(.*)*', redirect: '/home' }
 
 ]
@@ -17,7 +23,14 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
 
+  const isPublic = to.matched.some(record => record.meta.public);
+
+  if (!isPublic) {
+
     next();
-  })
+  } else {
+    next();
+  }
+});
 
 export default router
